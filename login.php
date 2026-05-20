7b16c4273b821bd4f6b8c4bf7f1a825af01467f7
106421717 <106421717@student.swin.edu.au>
2026-05-20 08:52:13 +0800

    Changed login.html to login.php

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?php include 'header.inc'; ?>
+
 <form method="post" action="process.php">
     <label for="username">Username:</label>
     <input type="text" name="username" required><br>
@@ -8,3 +10,5 @@
     <input type="hidden" name="token" value="abc123">
     <input type="submit" value="Login">
 </form>
+
+<?php include 'footer.inc'; ?>
